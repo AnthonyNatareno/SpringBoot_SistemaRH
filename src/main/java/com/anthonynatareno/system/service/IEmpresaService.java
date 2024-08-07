8308c12dceebf2dc324d6cfbde6cdf6d9a4d1d10
Bryanth87 <tgbryan14@gmail.com>
2024-08-07 11:57:43 -0600

    add:agregamos metodos a la interface de empresa

--- src/main/java/com/anthonynatareno/system/service/IEmpresaService.java
+++ src/main/java/com/anthonynatareno/system/service/IEmpresaService.java
@@ -1,5 +1,14 @@
 package com.anthonynatareno.system.service;
 
-public class IEmpresaService {
-    
+import java.util.List;
+import com.anthonynatareno.system.model.Empresa;
+
+public interface IEmpresaService {
+    public List<Empresa> listarEmpleados();
+ 
+    public Empresa buscarEmpresa(Integer idEmpleado);
+ 
+    public Empresa guardarEmpleados(Empresa empleados);
+ 
+    public void eliminarEmpresa(Empresa empleados);
 }
