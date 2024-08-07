84e7444c3216a63c41313131474bf7c674f5f71e
Bryanth87 <tgbryan14@gmail.com>
2024-08-07 11:25:36 -0600

    feat:Agregamos metodos a empleadoService y IEmpleadoService

--- src/main/java/com/anthonynatareno/system/service/EmpleadosService.java
+++ src/main/java/com/anthonynatareno/system/service/EmpleadosService.java
@@ -8,5 +8,29 @@ import com.anthonynatareno.system.model.Empleados;
 @Service
 
 public class EmpleadosService implements IEmpleadosService {
+
+    @Override
+    public List<Empleados> listarEmpleados() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarEmpleados'");
+    }
+
+    @Override
+    public Empleados buscarEmpleados(Integer idEmpleado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarEmpleados'");
+    }
+
+    @Override
+    public Empleados guardarEmpleados(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarEmpleados'");
+    }
+
+    @Override
+    public void eliminarEmpleados(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleados'");
+    }
     
 }
--- src/main/java/com/anthonynatareno/system/service/IEmpleadosService.java
+++ src/main/java/com/anthonynatareno/system/service/IEmpleadosService.java
@@ -3,13 +3,12 @@ package com.anthonynatareno.system.service;
 import java.util.List;
 import com.anthonynatareno.system.model.Empleados;
 
-public class IEmpleadosService {
- 
+public interface IEmpleadosService {
     public List<Empleados> listarEmpleados();
  
-    public Empleados buscarEmpleados (Integer idEmpleado);
+    public Empleados buscarEmpleados(Integer idEmpleado);
  
-    public Empleados guardarEmpleados (Empleados empleados);
+    public Empleados guardarEmpleados(Empleados empleados);
  
-    public void Empleados eliminarEmpleados(Empleados empleados);
+    public void eliminarEmpleados(Empleados empleados);
 }
