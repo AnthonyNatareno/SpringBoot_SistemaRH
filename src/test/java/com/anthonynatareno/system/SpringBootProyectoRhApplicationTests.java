e00a9231b4e41404b4a079909fc66a9dabb3caa7
Bryanth87 <tgbryan14@gmail.com>
2024-08-07 08:40:40 -0600

    add:proyecto agregado

---
+++ src/test/java/com/anthonynatareno/system/SpringBootProyectoRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonynatareno.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProyectoRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
