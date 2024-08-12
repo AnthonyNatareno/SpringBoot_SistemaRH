760ae7b8d1cc4cb48356c276ec69d63eb6db6550
Informatica <informatica@alumno.local>
2024-08-12 11:20:32 -0600

    add: añadimos el git de nuevo

---
+++ src/main/java/com/anthonynatareno/system/Controller/EmpleadoController.java
@@ -0,0 +1,10 @@
+package com.anthonynatareno.system.Controller;
+
+import java.util.logging.Logger;
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+
+public class EmpleadoController {
+    
+    private static final Logger logger = LoggerFactory.get(EmpleadoController.class);  
+}
