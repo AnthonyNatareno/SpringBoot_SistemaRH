084f91bb61591d7ef135656997ddd3575004a370
zzzDanielzzz <danieledusc@gmail.com>
2024-08-13 10:20:00 -0600

    feat: se agrego el metodo editar empleado

--- src/main/java/com/anthonynatareno/system/Controller/EmpleadosController.java
+++ src/main/java/com/anthonynatareno/system/Controller/EmpleadosController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anthonynatareno.system.Excepcion.EmpleadosException;
 import com.anthonynatareno.system.model.Empleados;
 import com.anthonynatareno.system.service.IEmpleadosService;
  
@@ -39,6 +43,17 @@ public class EmpleadosController {
             return iEmpleadosService.guardarEmpleados(empleados);
         }
 
-        
+        @PutMapping("/empleados/{id}")
+        public ResponseEntity <Empleados> editarEmpleados (@PathVariable Integer id, @RequestBody Empleados empleadosRecibido){
+            Empleados empleados = iEmpleadosService.buscarEmpleados(id);
+            if(empleados == null)
+                throw new EmpleadosException("El id recibido no existe");
+                empleados.setDpi(empleadosRecibido.getDpi());
+                empleados.setNombreCompleto(empleadosRecibido.getNombreCompleto());
+                empleados.setPuesto(empleadosRecibido.getPuesto());
+                empleados.setSalario(empleadosRecibido.getSalario());
+                iEmpleadosService.guardarEmpleados(empleados);
+                return ResponseEntity.ok(empleados);
+        }
 }
  
--- src/main/java/com/anthonynatareno/system/Excepcion/EmpleadosException.java
+++ src/main/java/com/anthonynatareno/system/Excepcion/EmpleadosException.java
@@ -10,4 +10,5 @@ public class EmpleadosException extends RuntimeException{
         super(mensaje);
     }
  
+
 }
