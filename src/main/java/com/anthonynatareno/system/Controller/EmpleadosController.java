5371c5fbaa84ef24151ffa203445955a6d543763
zzzDanielzzz <danieledusc@gmail.com>
2024-08-13 10:12:43 -0600

    feat: se agrego metodo agregar empleado

--- src/main/java/com/anthonynatareno/system/Controller/EmpleadosController.java
+++ src/main/java/com/anthonynatareno/system/Controller/EmpleadosController.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
@@ -28,6 +30,12 @@ public class EmpleadosController {
             empleado2.forEach((empleado -> logger.info(empleado.toString())));
             return empleado2;
  
+        }
+
+        @PostMapping("/empleadosA")
+        public Empleados agregarEmpleados (@RequestBody Empleados empleados){
+            logger.info("Empleado agregado");
+            return iEmpleadosService.guardarEmpleados(empleados);
         }
 }
  
